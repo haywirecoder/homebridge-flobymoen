8c6ed98d62ceccc790bcf8e079c706423f557917
haywirecoder <44388297+haywirecoder@users.noreply.github.com>
2021-08-15 00:03:21 -0400

    Add files via upload

--- src/debug/raw-dump.js
+++ src/debug/raw-dump.js
@@ -1,1 +1,21 @@
+const hap = require("hap-nodejs");
+var config = require('./config.json');
+var floclass = require('./flomain.js');
+var flo = new floclass (console,config);
 
+
+
+flo.init().then ( res_init => {
+   flo.discoverDevices().then (res_discover => {
+        flo.refreshAllDevices();
+    }) 
+    
+});
+
+
+flo.on('deviceupdate', function(data) {
+  
+	console.log("-------- Device Update ---------");
+	console.log("ID: ", data.id);
+	console.log("...");
+})
